5bb3326d95217c42298d19aa62fba000578b087b
kevinwiranata <kevin.a.wiranata@gmail.com>
2019-12-28 00:03:01 +0700

    changed file extensions

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import './App.css';
 import Welcome from './pages/Welcome';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
-import PythonProjects from './pages/Python_Project';
 import Error from './pages/Error';
 import Navbar from './components/Navbar';
 
-import {Route, Switch, Link} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
@@ -21,7 +20,6 @@ function App() {
       )}/>
       <Route exact path="/home" render={() => (<div><Navbar/><Home/></div>)}/>
       <Route exact path="/projects/" render={() => (<div><Navbar/><Projects/></div>)}/>
-      <Route exact path="/projects/:python" render={() => (<div><Navbar/><PythonProjects/></div>)}/>
       <Route exact path="/contact" render={() => (<div><Navbar/><Home/></div>)}/>
       <Route component = {Error}/>
     </Switch>
