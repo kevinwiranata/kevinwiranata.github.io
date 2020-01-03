69c7bfc9f440ebeb04ed185364a514617ae14e6e
kevinwiranata <kevin.a.wiranata@gmail.com>
2020-01-03 19:08:36 +0700

    netlify trial 3

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,7 +11,7 @@ export class Contact extends React.Component {
             <div id="main">
               <article id="contact">
                 <h2 class="major">Contact</h2>
-                <form method="POST" data-netlify="true">
+                <form name="contacts" method="POST" data-netlify="true">
                   <div class="fields">
                     <div class="field half">
                       <label for="name">Name</label>
