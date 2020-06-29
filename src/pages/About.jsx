596221a72459890de06cd13c485cdb12376a0822
kevinwiranata <kevin.a.wiranata@gmail.com>
2020-06-29 13:44:53 +0700

    fixed mobile media queries

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,62 +1,81 @@
-import React from 'react';
-import reactLogo from '../images/react.png';
-import jsLogo from '../images/js.png';
-import tsLogo from '../images/typescript.png';
-import nestLogo from '../images/nest.png';
-import htmlLogo from '../images/html.png';
-import cssLogo from '../images/css.png';
-import nodeLogo from '../images/node.png'
-import STMLogo from '../images/STM.png';
-import ArduinoLogo from '../images/Arduino.png';
-import EnergiaLogo from '../images/Energia.png';
-import PythonLogo from '../images/Python.png';
-import PandasLogo from '../images/pandas.png';
-import KerasLogo from '../images/keras.png'
-import F1Logo from '../images/f1.png';
-import atpLogo from '../images/atp.png';
-import wtaLogo from '../images/wta.png';
-import bwfLogo from '../images/bwf.png';
-import '../css/Pages.css';
+import React from "react";
+import reactLogo from "../images/react.png";
+import jsLogo from "../images/js.png";
+import tsLogo from "../images/typescript.png";
+import nestLogo from "../images/nest.png";
+import htmlLogo from "../images/html.png";
+import cssLogo from "../images/css.png";
+import nodeLogo from "../images/node.png";
+import STMLogo from "../images/STM.png";
+import ArduinoLogo from "../images/Arduino.png";
+import EnergiaLogo from "../images/Energia.png";
+import PythonLogo from "../images/Python.png";
+import PandasLogo from "../images/pandas.png";
+import KerasLogo from "../images/keras.png";
+import F1Logo from "../images/f1.png";
+import atpLogo from "../images/atp.png";
+import wtaLogo from "../images/wta.png";
+import bwfLogo from "../images/bwf.png";
+import "../css/Pages.css";
 
 const About = () => {
   return (
-    <div className="about-section" id = "about">
+    <div className="about-section" id="about">
       <div className="about-banner">
         <blockquote>
           <h1>About me!</h1>
         </blockquote>
         <div className="description">
-          <span>Second year Computer Engineering Student at UCLA | Mission-driven Full Stack Developer & Data Junkie</span>
-          <span className="code1">One day I'd like to <code>double up;</code> as a Randall Park <code>char acter;</code></span>
+          <span>
+            Second year Computer Engineering Student at UCLA | Mission-driven
+            Full Stack Developer & Data Junkie
+          </span>
+          <span className="code1">
+            One day I'd like to <code>double up;</code> as a Randall Park{" "}
+            <code>char acter;</code>
+          </span>
         </div>
       </div>
 
       <div class="container left">
         <div class="overlay">
-
-          <div class = "items head">
+          <div class="items head">
             <p>Full Stack Developer</p>
-            <hr/>
+            <hr />
           </div>
-          <div class = "items price">
-            <p class="new"><span className="area">Frontend: </span>Reactjs, HTML5, CSS3/SASS, Node.js</p>
-            <p class="new"><span className="area">Backend: </span>Nestjs, ExpressJS, Typescript, SQL, MongoDB</p>
-            <p class="old"><span className="area">Other: </span>C/C++, Git, Vim, Shell/Bash, TDD, x86-64, Verilog, Ansys, </p>
+          <div class="items price">
+            <p class="new">
+              <span className="area">Frontend: </span>Reactjs, HTML5, CSS3/SASS,
+              Node.js
+            </p>
+            <p class="new">
+              <span className="area">Backend: </span>Nestjs, ExpressJS,
+              Typescript, SQL, MongoDB
+            </p>
+            <p class="old">
+              <span className="area">Other: </span>C/C++, Git, Vim, Shell/Bash,
+              TDD, x86-64, Verilog, Ansys,{" "}
+            </p>
           </div>
           <div class="items cart">
             <i class="fa fa-shopping-cart"></i>
             <img src={reactLogo} alt="react" className="resize"></img>
-            <span/>
+            <span />
             <img src={jsLogo} alt="javascript" className="resize"></img>
-            <span/><span/>
+            <span />
+            <span />
             <img src={tsLogo} alt="typescript" className="resize"></img>
-            <span/><span/>
+            <span />
+            <span />
             <img src={nestLogo} alt="nest" className="resize"></img>
-            <br/><br/>
+            <br />
+            <br />
             <img src={htmlLogo} alt="html" className="resize"></img>
-            <span/><span/>
+            <span />
+            <span />
             <img src={cssLogo} alt="css" className="resize"></img>
-            <span/><span/>
+            <span />
+            <span />
             <img src={nodeLogo} alt="node" className="resize"></img>
           </div>
         </div>
@@ -64,26 +83,40 @@ const About = () => {
 
       <div class="container middle">
         <div class="overlay">
-          <div class = "items head">
+          <div class="items head">
             <p>Data & Robotics Enthusiast</p>
-            <hr/>
+            <hr />
           </div>
-          <div class = "items price">
-            <p class="old">Hardware: Arduino/Energia, Wireless Beaglebone, STM-Microelectronics Sensortile</p>
-            <p class="new">Software: Python (Pandas, Keras, Numpy, Neural Nets), SQL </p>
+          <div class="items price">
+            <p class="old">
+              Hardware: Arduino/Energia, Wireless Beaglebone,
+              STM-Microelectronics Sensortile
+            </p>
+            <p class="new">
+              Software: Python (Pandas, Keras, Numpy, Neural Nets), SQL{" "}
+            </p>
           </div>
           <div class="items cart">
             <i class="fa fa-shopping-cart"></i>
             <img src={STMLogo} alt="STM" className="resize"></img>
-            <span/><span/>
+            <span />
+            <span />
             <img src={ArduinoLogo} alt="Arduino" className="resize"></img>
-            <span/><span/>
+            <span />
+            <span />
             <img src={EnergiaLogo} alt="Energia" className="resize"></img>
-            <br/><br/>
+            <br />
+            <br />
             <img src={PythonLogo} alt="Python" className="resize"></img>
-            <span/><span/><span/><span/>
+            <span />
+            <span />
+            <span />
+            <span />
             <img src={KerasLogo} alt="Keras" className="resize"></img>
-            <span/><span/><span/><span/>
+            <span />
+            <span />
+            <span />
+            <span />
             <img src={PandasLogo} alt="Pandas" className="resize"></img>
           </div>
         </div>
@@ -91,30 +124,38 @@ const About = () => {
 
       <div class="container right">
         <div class="overlay">
-          <div class = "items head">
+          <div class="items head">
             <p>Pun Trivia Facts!</p>
-            <hr/>
+            <hr />
           </div>
-          <div class = "items price">
-            <p class="old">I'm a decent petrolhead, but sometimes car puns can be <i>exhausting</i>...</p>
-            <p class="old">I love to talk - part of me is just being so chatty that sometimes I cause a <i>racquet</i>...</p>
-            <p class="new">tldr; I'm a <i>huge</i> fan of Motorsport (F1, WEC & WRC), Tennis (ATP & WTA) and Badminton!</p>
-
+          <div class="items price">
+            <p class="old">
+              I'm a decent petrolhead, but sometimes car puns can be{" "}
+              <i>exhausting</i>...
+            </p>
+            <p class="old">
+              I love to talk - part of me is just being so chatty that sometimes
+              I cause a <i>racquet</i>...
+            </p>
+            <p class="new">
+              tldr; I'm a <i>huge</i> fan of Motorsport (F1, WEC & WRC), Tennis
+              (ATP & WTA) and Badminton!
+            </p>
           </div>
           <div class="items cart">
             <i class="fa fa-shopping-cart"></i>
             <img src={F1Logo} alt="F1" className="resize"></img>
             <img src={atpLogo} alt="ATP" className="resize"></img>
-            <span/>
+            <span />
             <img src={wtaLogo} alt="WTA" className="resize"></img>
-            <span/><span/>
+            <span />
+            <span />
             <img src={bwfLogo} alt="BWF" className="resize"></img>
           </div>
         </div>
       </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default About
+export default About;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,103 +1,138 @@
-import React from 'react';
-import '../css/Pages.css';
-import 'react-animated-slider/build/horizontal.css';
-import alterra from '../images/alterra.png';
-import speakly from '../images/speakly.ico';
-import lahacks from '../images/lahacks.jpeg';
-import acm from '../images/acmai.jpg';
-import artage from '../images/artage.jpg';
-import cloud from '../images/cloud.png'
+import React from "react";
+import "../css/Pages.css";
+import "react-animated-slider/build/horizontal.css";
+import alterra from "../images/alterra.png";
+import speakly from "../images/speakly.ico";
+import artage from "../images/artage.jpg";
+import cloud from "../images/cloud.png";
 
 export class Portfolio extends React.Component {
-  state = {}
+  state = {};
   render() {
     return (
-      <div className="portfolio-section"  id = "portfolio">
+      <div className="portfolio-section" id="portfolio">
         <div className="portfolio-banner">
           <blockquote className="portfolio">
             <h1>Portfolio</h1>
           </blockquote>
           <div className="description">
-            <span>Sometimes I like to make things, occasionally they actually end up being pretty cool.</span> 
-            <span className="code2"> Take a <code>short</code> <code>walk(around);</code> I don't <code>byte!</code></span>
+            <span>
+              Sometimes I like to make things, occasionally they actually end up
+              being pretty cool.
+            </span>
+            <span className="code2">
+              {" "}
+              Take a <code>short</code> <code>walk(around);</code> I don't{" "}
+              <code>byte!</code>
+            </span>
           </div>
         </div>
 
         <div class="cards">
-          <div class="card-container"> 
-            <div class="card"><a href="https://www.alterra.id/about-us/" target="_blank" rel="noopener noreferrer">
+          <div class="card-container">
+            <div class="card">
+              <a
+                href="https://www.alterra.id/about-us/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <div class="card--display">
-                  <img src={alterra} alt="alterra" className="image"/>
-                  <h1 class = "h1-portfolio">Alterra</h1>
-                  <h3>Software Engineering Intern</h3>
-                  <p>July - September 2019</p>
-                </div>
-                <div class="card--hover">
                   <h2 className="card-title">Alterra</h2>
-                  <img src={alterra} alt="alterra" className="image-hover1"/>
+                  <img src={alterra} alt="alterra" className="image-hover1" />
                   <h4>Software Engineering Intern | July - Sept 2019</h4>
-                  <ul class = "card-text">
-                    <li>Built a server-side application for a flexible billing system using nestJS and Express.js, reducing biller API integration time by 90%</li>
-                    <br/>
-                    <li>Researched and implemented Node.js microservices (rabbitMQ, redis, amqp) in typescript to provide asynchronous operations to external system configuration and processes</li>
-                    <br/>
-                    <li>Managed gitflow using Gitlab CI/CD for staging, and Sonarqube + GitPrime for code analysis</li>
+                  <ul class="card-text">
+                    <li>
+                      Built a server-side application for a flexible billing
+                      system using nestJS and Express.js, reducing biller API
+                      integration time by 90%
+                    </li>
+                    <br />
+                    <li>
+                      Researched and implemented Node.js microservices
+                      (rabbitMQ, redis, amqp) in typescript to provide
+                      asynchronous operations to external system configuration
+                      and processes
+                    </li>
+                    <br />
+                    <li>
+                      Managed gitflow using Gitlab CI/CD for staging, and
+                      Sonarqube + GitPrime for code analysis
+                    </li>
                   </ul>
-                  <br/>
-                  <p class = "card-text"><strong>Tech Stack:</strong> Typescript, nestJS, Express.JS, mongoDB, redis, rabbitMQ </p>
-                  <p class = "card-text"><strong>Other:</strong> TDD, Unit-testing, End-to-End testing</p>
-                </div></a>
+                  <br />
+                  <p class="card-text">
+                    <strong>Tech Stack:</strong> Typescript, nestJS, Express.JS,
+                    mongoDB, redis, rabbitMQ, Jest (unit/integration tests){" "}
+                  </p>
+                </div>
+              </a>
               <div class="card--border"></div>
             </div>
           </div>
-          <div class="card-container"> 
-            <div class="card"><a href="https://github.com/kevinwiranata/Speakly" target="_blank" rel="noopener noreferrer">
+          <div class="card-container">
+            <div class="card">
+              <a
+                href="https://github.com/kevinwiranata/Speakly"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <div class="card--display">
-                <img src={speakly} alt="speakly" className="image"/>
-                <img src={lahacks} alt="lahacks" className="image-right"/>
-                  <h1 class = "h1-portfolio">Speakly</h1>
-                  <h3>Public Speaking Algorithm</h3>
-                  <p>March 2019 (LA Hacks 2019)</p>
-                </div>
-                <div class="card--hover">
-                  <h2 className="card-title">Speakly</h2> 
-                  <img src={speakly} alt="speakly" className="image-hover2"/>
-                  <img src={cloud} alt="gcloud" className="image-hover1"/>
+                  <h2 className="card-title">Speakly</h2>
+                  <img src={speakly} alt="speakly" className="image-hover2" />
+                  <img src={cloud} alt="gcloud" className="image-hover1" />
                   <h4>Awarded Best use of Google Cloud API (LA Hacks 2019)</h4>
-                  <ul class = "card-text">
-                    <li>Speakly uses Google's Speech-To-Text, Natural Language, and Vision API to track the speaker’s facial movements and emotions</li>
-                    <br/>
-                    <li>The program then utilizes this information and runs an algorithm to grade and highlight specific areas the speaker needs to improve</li>
+                  <ul class="card-text">
+                    <li>
+                      Speakly uses Google's Speech-To-Text, Natural Language,
+                      and Vision API to track the speaker’s facial movements and
+                      emotions
+                    </li>
+                    <br />
+                    <li>
+                      The program then utilizes this information and runs an
+                      algorithm to grade and highlight specific areas the
+                      speaker needs to improve
+                    </li>
                   </ul>
-                  <br/>
-                  <p class = "card-text"><strong>Tech Stack:</strong> Python, Google Cloud API (Vision, Speech-to-Text, NLP), HTML/CSS, PHP  </p>
-                </div></a>
+                  <br />
+                  <p class="card-text">
+                    <strong>Tech Stack:</strong> Python, Google Cloud API
+                    (Vision, Speech-to-Text, NLP), HTML/CSS, PHP{" "}
+                  </p>
+                </div>
+              </a>
               <div class="card--border"></div>
             </div>
           </div>
-          <div class="card-container"> 
-            <div class="card"><a href="https://github.com/kevinwiranata/Project-Artage" target="_blank" rel="noopener noreferrer">
+          <div class="card-container">
+            <div class="card">
+              <a
+                href="https://github.com/kevinwiranata/Project-Artage"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <div class="card--display">
-                <img src={acm} alt="acm" className="image"/>
-                  <h1 class = "h1-portfolio">Project Artage</h1>
-                  <h3>Convolution Neural Network</h3>
-                  <p>February 2019</p>
-                </div>
-                <div class="card--hover">
                   <h2>Project Artage</h2>
-                  <h4 class = "card-text">A Convolutional Neural Network that looks for particular features in paintings and predicts the year artwork was made</h4>
-                  <img src={artage} alt="artage" className="image-hover3"/>
-                  <br/>
-                  <p class = "card-text"><strong>Tech Stack:</strong> Python (numpy, pandas, keras, scipy), HTML, PHP </p>
-                </div></a>
+                  <h4>February 2019</h4>
+                  <h5 class="card-text">
+                    A Convolutional Neural Network that looks for particular
+                    features in paintings and predicts the year artwork was made
+                  </h5>
+                  <img src={artage} alt="artage" className="image-hover3" />
+                  <br />
+                  <p class="card-text">
+                    <strong>Tech Stack:</strong> Python (numpy, pandas, keras,
+                    scipy), HTML, PHP{" "}
+                  </p>
+                </div>
+              </a>
               <div class="card--border"></div>
             </div>
           </div>
         </div>
       </div>
-
-    )
+    );
   }
 }
 
-export default Portfolio
+export default Portfolio;
