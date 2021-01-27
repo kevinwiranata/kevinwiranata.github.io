4157c2619c6339614d40da5a24406f57af941a8f
kevinwiranata <kevin.a.wiranata@gmail.com>
2021-01-28 02:11:15 +0700

    update resume v2

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
         <Contact/>
       </div>)}/>
       <Route exact path="/resume" component={() => { 
-        window.location.href = 'https://d1b10bmlvqabco.cloudfront.net/user_resumes/jqzv730o4vt3pq/jqzv730o4vt3pq_1611757907.pdf'; 
+        window.location.href = 'https://d1b10bmlvqabco.cloudfront.net/user_resumes/jqzv730o4vt3pq/jqzv730o4vt3pq_1611774601.pdf'; 
         return null;
       }}/>
       <Route component = {Error}/>
