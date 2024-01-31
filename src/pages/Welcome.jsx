59b5135d8d218929c0ce5375b9063aecff8e9cef
kevinwiranata <kevin.a.wiranata@gmail.com>
2024-01-31 02:13:07 -0500

    update welcome page

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,37 +1,36 @@
-import React from 'react'
-import '../css/Star.css'
-import {Link} from 'react-router-dom'
-import '../css/Button.css'
-import logo from '../images/logo3.png'
-import SocialFollow from '../components/SocialFollow'
+import React from "react";
+import "../css/Star.css";
+import { Link } from "react-router-dom";
+import "../css/Button.css";
+import logo from "../images/logo3.png";
+import SocialFollow from "../components/SocialFollow";
 
 const Welcome = () => {
-  return (
-    <div>
-      <div id='stars1'></div>
-      <div id='stars2'></div>
-      <div id='stars3'></div>
-      <div className='title1'>
-        <div>Hi I'm <span className='name'>Kevin Wiranata</span></div>
-        <div>I'm a Computer Engineer at UCLA
-         </div>
-        <br/>
-        <img id="optionalstuff" src={logo} alt = "Home"/>
-      </div>
-      <div id='title'>
-        <div className='box-1'>
-          <Link to='/home' className = 'btn btn-three'>
-          Enter
-          </Link>
-        </div>
-        <br/>
-        <SocialFollow>
-        </SocialFollow>
-      </div>
+	return (
+		<div>
+			<div id="stars1"></div>
+			<div id="stars2"></div>
+			<div id="stars3"></div>
+			<div className="title1">
+				<div>
+					Hi! I'm <span className="name">Kevin Wiranata</span>
+				</div>
+				<div>I'm a Grad CS Student at Cornell Tech</div>
+				<div>Previously, I was a Software Engineer at Verizon and Yahoo</div>
+				<br />
+				<img id="optionalstuff" src={logo} alt="Home" />
+			</div>
+			<div id="title">
+				<div className="box-1">
+					<Link to="/home" className="btn btn-three">
+						Enter
+					</Link>
+				</div>
+				<br />
+				<SocialFollow></SocialFollow>
+			</div>
+		</div>
+	);
+};
 
-
-    </div>
-  )
-}
-
-export default Welcome
+export default Welcome;
